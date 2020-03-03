f22782e9b7fde55495fb329dcb7499c878c0981e
Joan Sebastian Sandoval Parra <joan.sebas.sanpa@gmail.com>
2020-03-03 13:58:44 -0500

    aja y ahora

---
+++ caida.py
@@ -0,0 +1,11 @@
+print("Este programa calcula la altura")
+h=input("introduzca el valor de h, h= ")
+t=input("introduzca el valor de t, t= ")
+
+h,t=float(h),float(t)
+
+x=h-(9.8/2)*(t**2)
+print("la altura final es: h=",x)
+
+
+
